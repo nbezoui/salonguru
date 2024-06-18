7d84bf8da60bbe1e865e3a259753545fe25a4593
Najla <bziouechnajla@gmail.com>
2024-06-18 14:09:05 +0200

    finish styling

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,7 +1,6 @@
-// components/cart/cart.component.ts
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Observable, combineLatest, map, take } from 'rxjs';
+import { Observable, combineLatest, Subscription, map, take } from 'rxjs';
 import { CartItem, Product } from '../../models/cart-item';
 import { addToCartSuccess, clearCart, removeFromCart } from 'src/app/store/actions/cart.actions';
 import { AppState } from 'src/app/store/state/app.state';
@@ -12,21 +11,25 @@ import { LocalStorageService } from 'src/app/services/local-storage.service';
 import { selectAllProducts } from 'src/app/store/selectors/product.selector';
 import { CheckoutPayload } from 'src/app/store/reducers/checkout.reducer';
 
-
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.scss']
 })
-export class CartComponent implements OnInit {
+export class CartComponent implements OnInit, OnDestroy {
   cartItems$: Observable<CartItem[]> = new Observable();
   total$: Observable<number> = new Observable();
-  allProducts$: Observable<Product[]> = new Observable();;
+  allProducts$: Observable<Product[]> = new Observable();
+  private checkoutSubscription: Subscription = new Subscription();
 
-  constructor(private store: Store<AppState>, private readonly router: Router, private localStorage: LocalStorageService) {}
+  constructor(
+    private store: Store<AppState>,
+    private readonly router: Router,
+    private localStorage: LocalStorageService
+  ) {}
 
   ngOnInit(): void {
-    this.store.dispatch(addToCartSuccess({items: this.localStorage.getCartItems()}))
+    this.store.dispatch(addToCartSuccess({ items: this.localStorage.getCartItems() }));
     this.cartItems$ = this.store.select(selectCartItems);
     this.allProducts$ = this.store.select(selectAllProducts);
     this.total$ = this.store.select(selectCartTotal);
@@ -41,15 +44,15 @@ export class CartComponent implements OnInit {
   }
 
   checkout(): void {
-    combineLatest(
-      this.cartItems$.pipe(
-      map(cartItems => this.getCheckoutArray(cartItems)),
-      take(1),
-    ), this.allProducts$.pipe(
-      take(1),
-    ))
-    .subscribe(([checkout, allProducts]) => this.store.dispatch(initiateCheckout({ items: checkout, products: allProducts })) );
-    this.router.navigate(["checkout"])
+    this.checkoutSubscription.add(
+      combineLatest([
+        this.cartItems$.pipe(map(cartItems => this.getCheckoutArray(cartItems)), take(1)),
+        this.allProducts$.pipe(take(1))
+      ]).subscribe(([checkout, allProducts]) => {
+        this.store.dispatch(initiateCheckout({ items: checkout, products: allProducts }));
+        this.router.navigate(['checkout']);
+      })
+    );
   }
 
   private getCheckoutArray(cartItems: CartItem[]): CheckoutPayload[] {
@@ -58,5 +61,8 @@ export class CartComponent implements OnInit {
       quantity: item.quantity
     }));
   }
-  
+
+  ngOnDestroy(): void {
+    this.checkoutSubscription.unsubscribe();
+  }
 }
